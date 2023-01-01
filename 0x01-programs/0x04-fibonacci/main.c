310e2242fe781feea5134d5f85f6283a904965cf
Eusebio Simango <eusebiosimango14@gmail.com>
2023-01-02 01:42:42 +0200

    BugFixing: int to long && %d to %ld

--- 0x01-programs/0x04-fibonacci/main.c
+++ 0x01-programs/0x04-fibonacci/main.c
@@ -2,15 +2,16 @@
 
 int main(void)
 {
-	int current, prev, _prev, _current, i;
+	long current, prev, _current;
+	int i;
 
 	prev = 1;
 	current = 2;
 
 	for (i = 0; i < 90; i++){
 		if (i == 0)
-			printf("%d ", prev);
-		printf("%d ", current);
+			printf("%ld ", prev);
+		printf("%ld ", current);
 		_current = current;
 		current = prev + current;
 		prev = _current;

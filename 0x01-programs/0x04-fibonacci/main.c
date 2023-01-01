c2e8ceb640df69b3fd855f882786f3f4b21cf48e
Eusebio Simango <eusebiosimango14@gmail.com>
2023-01-02 01:53:45 +0200

    refactoring

--- 0x01-programs/0x04-fibonacci/main.c
+++ 0x01-programs/0x04-fibonacci/main.c
@@ -2,17 +2,19 @@
 
 int main(void)
 {
-	long current, prev, _current;
-	int i;
+	long current, prev;
+	int counter, x;
 
 	prev = 1;
 	current = 2;
 
-	for (i = 0; i < 90; i++){
-		if (i == 0)
-			printf("%ld ", prev);
+	printf("Sequence of how many numbers: ");
+	scanf("%d", &x);
+	
+	printf("%ld ", prev);
+	for (counter = 1; counter < x; counter++){
 		printf("%ld ", current);
-		_current = current;
+		int _current = current;
 		current = prev + current;
 		prev = _current;
 	}

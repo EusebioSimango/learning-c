5eb934f27341263202e2baf18b42fec5977be8e9
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2023-03-29 12:19:52 +0200

    Add: logs and loop to the array

--- memoryAllocation/calloc.c
+++ memoryAllocation/calloc.c
@@ -5,13 +5,25 @@ int main(void){
     int *p;
     int n, i;
     
-    n = 10;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
     p = (int *)calloc(n, sizeof(int));
     if (p == NULL){
        printf("Memory not allocated.\n");
        exit(0);
     } else {
-      printf("Memory allocated successfully.\n");
+      printf("Memory allocated successfully using calloc.\n");
+      printf("Enter the elements of the array.\n");
+      for (i = 0; i < n; i++){
+	      printf("pos %d: ", i);
+	      scanf("%d", p+i);
+      }
+      printf("The elements: ");
+      for (i = 0; i < n; i++) {
+          if (i == (n-1))
+             return (printf("%d\n", *(p+i)));
+          printf("%d | ", *(p+i));
+      }
       free(p);
       
     }
